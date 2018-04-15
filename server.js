c2419327daa67f0043b7f0ea499a379cfcdae5ef
Gabe <gperry22@gmail.com>
2018-04-14 22:16:39 -0400

    updates

--- server.js
+++ server.js
@@ -41,9 +41,7 @@ mongoose.connect("mongodb://localhost/mongoScrapper", {
   // useMongoClient: true
 });
 
-mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds229465.mlab.com:29465/heroku_bl31fws6", {
-  // useMongoClient: true
-});
+mongoose.connect(MONGODB_URL)
 
 // Start the server
 app.listen(PORT, function () {
